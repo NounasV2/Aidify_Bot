086ad0ea0f99eb631f27dfcdf00cc279e2c14f91
NounasV2 <49840132+NounasV2@users.noreply.github.com>
2019-04-21 18:58:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login("NTY5NTQ2MDY5OTQwMzcxNDc3.XLyXNw.k0whgSyi1QErXLKWPJbInXi8mXo");
+client.login("NTY5NTY2OTA3NzkxODM1MTM2.XLygmQ.7b5piT00E-0tPSnNKM8bwQqRkOU");
